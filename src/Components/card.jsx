ff5bb18c481dbf03a9a448a76aebf61ca2ac3926
BettinaHallinger <bettina.hallinger@htl-saalfelden.at>
2022-05-04 10:39:57 +0200

    Endpage

--- src/Components/card.jsx
+++ src/Components/card.jsx
@@ -5,7 +5,7 @@ import Ueberschrift2 from "./ueberschrift2";
 
 function Card(props) {
   return (
-    <div class="w-full rounded-xl overflow-hidden shadow-lg bg-gray1 ">
+    <div class="w-full rounded-xl overflow-hidden shadow-lg bg-gray1">
       <div class="h-60">
         <img class="w-full h-full object-cover" src={props.bild || bild} alt="Papa Roy"></img>
       </div>
